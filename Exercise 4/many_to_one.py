927ff6ef90c5b2e080b1678cb26717e09db65e36
Odin Kvarving <odinkva@stud.ntnu.no>
2021-09-23 21:17:06 +0200

    Finished model with many to many architecture (task a)

--- Exercise 4/many_to_one.py
+++ Exercise 4/many_to_one.py
@@ -26,24 +26,56 @@ class LongShortTermMemoryModel(nn.Module):
 
 
 char_encodings = [
-    [1., 0., 0., 0., 0.],  # ' '
-    [0., 1., 0., 0., 0.],  # 'h'
-    [0., 0., 1., 0., 0.],  # 'e'
-    [0., 0., 0., 1., 0.],  # 'l'
-    [0., 0., 0., 0., 1.],  # 'o'
+    [1., 0., 0., 0., 0., 0., 0., 0., 0.],  # ' '
+    [0., 1., 0., 0., 0., 0., 0., 0., 0.],  # 'h'
+    [0., 0., 1., 0., 0., 0., 0., 0., 0.],  # 'e'
+    [0., 0., 0., 1., 0., 0., 0., 0., 0.],  # 'l'
+    [0., 0., 0., 0., 1., 0., 0., 0., 0.],  # 'o'
+    [0., 0., 0., 0., 0., 1., 0., 0., 0.],  # 'w'
+    [0., 0., 0., 0., 0., 0., 1., 0., 0.],  # 'r'
+    [0., 0., 0., 0., 0., 0., 0., 1., 0.],  # 'd'
+    [0., 0., 0., 0., 0., 0., 0., 0., 1.],  # '-'
+
+
 ]
 encoding_size = len(char_encodings)
 
-index_to_char = [' ', 'h', 'e', 'l', 'o']
+index_to_char = [' ', 'h', 'e', 'l', 'o', 'w', 'r', 'd', '-']
 
-x_train = torch.tensor([[char_encodings[0]], [char_encodings[1]], [char_encodings[2]], [char_encodings[3]], [char_encodings[3]],
-                        [char_encodings[4]]])  # ' hello'
-y_train = torch.tensor([char_encodings[1], char_encodings[2], char_encodings[3], char_encodings[3], char_encodings[4], char_encodings[0]])  # 'hello '
+x_train = torch.tensor([
+    [char_encodings[0]],   # ' '
+    [char_encodings[1]],   # 'h'
+    [char_encodings[2]],   # 'e'
+    [char_encodings[3]],   # 'l'
+    [char_encodings[3]],   # 'l'
+    [char_encodings[4]],   # 'o'
+    [char_encodings[8]],   # '-'
+    [char_encodings[5]],   # 'w'
+    [char_encodings[4]],   # 'o'
+    [char_encodings[6]],   # 'r'
+    [char_encodings[3]],   # 'l'
+    [char_encodings[7]],  # 'd'
+    ])  # ' hello world'
+
+y_train = torch.tensor([
+     char_encodings[1],  # 'h'
+     char_encodings[2],  # 'e'
+     char_encodings[3],  # 'l'
+     char_encodings[3],  # 'l'
+     char_encodings[4],  # 'o'
+     char_encodings[8],  # '-'
+     char_encodings[5],  # 'w'
+     char_encodings[4],  # 'o'
+     char_encodings[6],  # 'r'
+     char_encodings[3],  # 'l'
+     char_encodings[7],  # 'd'
+     char_encodings[0],  # ' '
+     ])  # 'hello world '
 
 model = LongShortTermMemoryModel(encoding_size)
 
-optimizer = torch.optim.RMSprop(model.parameters(), 0.001)
-for epoch in range(500):
+optimizer = torch.optim.RMSprop(model.parameters(), 0.005)
+for epoch in range(2000):
     model.reset()
     model.loss(x_train, y_train).backward()
     optimizer.step()
